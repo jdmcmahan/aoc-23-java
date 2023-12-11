94d2c261c2631e6dbfb8c89b76e04dbb99811f01
Donnie McMahan <jmcmaha1@gmail.com>
2023-12-11 10:59:13 -0600

    Clean up imports

--- aoc-10-pipemaze/src/test/java/com/aoc/jdmcmahan/pipemaze/PipeMazeNavigationTest.java
+++ aoc-10-pipemaze/src/test/java/com/aoc/jdmcmahan/pipemaze/PipeMazeNavigationTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
